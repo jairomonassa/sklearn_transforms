69d4e2864fc7eda9713a59e1e4783f319642d1cb
jairomonassa <65565163+jairomonassa@users.noreply.github.com>
2020-08-20 00:02:32 -0300

    Update sklearn_transformers.py
    jairo neder notas maior que 10

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,27 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+    def transforma_perfil (valor):
+        # MUDADO O TARGET PARA BINÁRIO, TALVEZ NÃO USE
+        if valor == 'EXATAS':
+            return 0
+           
+        elif  valor == 'DIFICULDADE':
+              return 1
+          
+        elif  valor =='HUMANAS':
+             return 2
+       
+        elif  valor =='MUITO_BOM':
+              return 3
+             
+        elif  valor =='EXCELENTE':
+              return 4
+            
+    def transforma_nota (valor):
+        #NOTAS QUE NÃO SÃO MAIOR QUE 10
+        if valor > 10.0:
+            return 10.0
+        else:
+            return valor
